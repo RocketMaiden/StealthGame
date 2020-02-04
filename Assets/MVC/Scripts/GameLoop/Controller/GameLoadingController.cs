b9ce743866f37898b323972f97f179f6b260948c
Elizaveta Suzdalskaia <e.suzdalskaia@room8studio.com>
2020-02-04 15:46:55 +0200

    added materials and light

---
+++ Assets/MVC/Scripts/GameLoop/Controller/GameLoadingController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.MVC.Scripts.GameLoop.Controller
+{
+    public class GameLoadingController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
